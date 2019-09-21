22a9acaeed053b3a8b995d74997558eabc25f71c
Pieter Delobelle <pieter.delobelle@gmail.com>
2019-09-21 17:04:51 +0200

    Settings page with tabs instead of long list

--- src/components/SettingsView.vue
+++ src/components/SettingsView.vue
@@ -39,141 +39,172 @@
         </ul>
       </div>
       <div class="col-md-8 order-md-1">
-        <h4 class="mb-3">Profile information</h4>
-        <form class="needs-validation" novalidate>
-          <div class="row">
-            <div class="col-md-2 mb-3">
-              <label for="firstName">Title</label>
-              <input type="text" class="form-control" id="firstName" placeholder value />
-            </div>
-            <div class="col-md-5 mb-3">
-              <label for="firstName">First name</label>
-              <input type="text" class="form-control" id="firstName" placeholder value required />
-              <div class="invalid-feedback">Valid first name is required.</div>
-            </div>
-            <div class="col-md-5 mb-3">
-              <label for="lastName">Last name</label>
-              <input type="text" class="form-control" id="lastName" placeholder value required />
-              <div class="invalid-feedback">Valid last name is required.</div>
-            </div>
-          </div>
+        <b-tabs pills content-class="mt-3">
+          <b-tab title="Profile information" active>
+            <div class="tab-content" id="pills-tabContent">
+              <div
+                class="tab-pane fade show active"
+                id="pills-home"
+                role="tabpanel"
+                aria-labelledby="pills-home-tab"
+              >
+                <form class="needs-validation" novalidate>
+                  <div class="row">
+                    <div class="col-md-2 mb-3">
+                      <label for="firstName">Title</label>
+                      <input type="text" class="form-control" id="firstName" placeholder value />
+                    </div>
+                    <div class="col-md-5 mb-3">
+                      <label for="firstName">First name</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="firstName"
+                        placeholder
+                        value
+                        required
+                      />
+                      <div class="invalid-feedback">Valid first name is required.</div>
+                    </div>
+                    <div class="col-md-5 mb-3">
+                      <label for="lastName">Last name</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="lastName"
+                        placeholder
+                        value
+                        required
+                      />
+                      <div class="invalid-feedback">Valid last name is required.</div>
+                    </div>
+                  </div>
 
-          <div class="mb-3">
-            <label for="username">Username</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text">@</span>
+                  <div class="mb-3">
+                    <label for="username">Username</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">@</span>
+                      </div>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="username"
+                        placeholder="Username"
+                        required
+                      />
+                      <div class="invalid-feedback" style="width: 100%;">Your username is required.</div>
+                    </div>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="email">
+                      Email
+                      <span class="text-muted">(Optional)</span>
+                    </label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="email"
+                      placeholder="you@example.com"
+                    />
+                    <div
+                      class="invalid-feedback"
+                    >Please enter a valid email address if you want email notifications.</div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-md-5 mb-3">
+                      <label for="country">Language</label>
+                      <select class="custom-select d-block w-100" id="language" required>
+                        <option value>English</option>
+                      </select>
+                      <div class="invalid-feedback">Please select a valid language.</div>
+                    </div>
+                    <div class="col-md-7 mb-3">
+                      <label for="state">Time zone</label>
+                      <select class="custom-select d-block w-100" id="timezone" required>
+                        <option value>Set the time zone automagically</option>
+                        <option>(UTC+01:00) Brussels, Copenhagen, Madrid, Paris</option>
+                      </select>
+                      <div class="invalid-feedback">Please provide a time zone.</div>
+                    </div>
+                  </div>
+                  <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="email-notifications" />
+                    <label
+                      class="custom-control-label"
+                      for="email-notifications"
+                    >Forward unread messages to my email address</label>
+                  </div>
+                  <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="show-online" />
+                    <label
+                      class="custom-control-label"
+                      for="show-online"
+                    >Show an online indicator based on my active devices</label>
+                  </div>
+                  <button
+                    class="my-4 btn btn-outline-primary btn-lg btn-block"
+                    type="submit"
+                  >Update my profile</button>
+                </form>
               </div>
-              <input type="text" class="form-control" id="username" placeholder="Username" required />
-              <div class="invalid-feedback" style="width: 100%;">Your username is required.</div>
             </div>
-          </div>
+          </b-tab>
+          <b-tab title="Password">
+            <form>
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <label for="password">Password</label>
+                  <input type="password" class="form-control" id="password" placeholder required />
+                  <div
+                    class="invalid-feedback"
+                  >A password is kind of required when changing a password</div>
+                </div>
+                <div class="col-md-6 mb-3">
+                  <label for="password-confirm">Retype password</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="password-confirm"
+                    placeholder
+                    required
+                  />
+                  <div class="invalid-feedback">Both passwords don't match</div>
+                </div>
+              </div>
 
-          <div class="mb-3">
-            <label for="email">
-              Email
-              <span class="text-muted">(Optional)</span>
-            </label>
-            <input type="email" class="form-control" id="email" placeholder="you@example.com" />
-            <div
-              class="invalid-feedback"
-            >Please enter a valid email address if you want email notifications.</div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-5 mb-3">
-              <label for="country">Language</label>
-              <select class="custom-select d-block w-100" id="language" required>
-                <option value>English</option>
-              </select>
-              <div class="invalid-feedback">Please select a valid language.</div>
+              <button
+                class="mt-4 btn btn-outline-primary btn-lg btn-block"
+                type="submit"
+              >Update password</button>
+            </form>
+          </b-tab>
+          <b-tab title="Profile picture">
+            <div class="row">
+              <div class="col-md-12 mb-3">
+                <label for="file">Password</label>
+                <b-form-file
+                  v-model="file"
+                  :state="Boolean(file)"
+                  id="file"
+                  placeholder="Choose a file..."
+                  drop-placeholder="Drop file here..."
+                ></b-form-file>
+                <div class="invalid-feedback">Something's wrong with this file...</div>
+              </div>
             </div>
-            <div class="col-md-7 mb-3">
-              <label for="state">Time zone</label>
-              <select class="custom-select d-block w-100" id="timezone" required>
-                <option value>Set the time zone automagically</option>
-                <option>(UTC+01:00) Brussels, Copenhagen, Madrid, Paris</option>
-              </select>
-              <div class="invalid-feedback">Please provide a time zone.</div>
-            </div>
-          </div>
-          <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="email-notifications" />
-            <label
-              class="custom-control-label"
-              for="email-notifications"
-            >Forward unread messages to my email address</label>
-          </div>
-          <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="show-online" />
-            <label
-              class="custom-control-label"
-              for="show-online"
-            >Show an online indicator based on my active devices</label>
-          </div>
-          <button
-            class="mt-4 btn btn-outline-primary btn-lg btn-block"
-            type="submit"
-          >Update my profile</button>
-        </form>
-        <form>
-          <hr class="mb-4" />
 
-          <h4 class="mb-3">Password</h4>
-
-          <div class="row">
-            <div class="col-md-6 mb-3">
-              <label for="password">Password</label>
-              <input type="password" class="form-control" id="password" placeholder required />
-              <div class="invalid-feedback">A password is kind of required when changing a password</div>
-            </div>
-            <div class="col-md-6 mb-3">
-              <label for="password-confirm">Retype password</label>
-              <input
-                type="password"
-                class="form-control"
-                id="password-confirm"
-                placeholder
-                required
-              />
-              <div class="invalid-feedback">Both passwords don't match</div>
-            </div>
-          </div>
-
-          <button
-            class="mt-4 btn btn-outline-primary btn-lg btn-block"
-            type="submit"
-          >Update password</button>
-        </form>
-        <div>
-          <hr class="mb-4" />
-
-          <h4 class="mb-3">Profile picture</h4>
-
-          <div class="row">
-            <div class="col-md-12 mb-3">
-              <label for="file">Password</label>
-              <b-form-file
-                v-model="file"
-                :state="Boolean(file)"
-                id="file"
-                placeholder="Choose a file..."
-                drop-placeholder="Drop file here..."
-              ></b-form-file>
-              <div class="invalid-feedback">Something's wrong with this file...</div>
-            </div>
-          </div>
-
-          <button
-            class="mt-4 btn btn-outline-primary btn-lg btn-block"
-            type="submit"
-            v-on:click="uploadProfile"
-          >Update profile picture</button>
-        </div>
+            <button
+              class="mt-4 btn btn-outline-primary btn-lg btn-block"
+              type="submit"
+              v-on:click="uploadProfile"
+            >Update profile picture</button>
+          </b-tab>
+        </b-tabs>
       </div>
     </div>
-
-    <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
   </div>
 </template>
 
